7f848e889dfb8aa4923d222ffaea7a784139889a
Arthur Aguiar <sepulven40028922@gmail.com>
2024-04-24 13:00:36 +0100

    Clean the project structure. Prepare for merge into the main.

--- srcs/ServerContext.cpp
+++ srcs/ServerContext.cpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ServerContext.cpp                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: asepulve <asepulve@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 16:31:58 by asepulve          #+#    #+#             */
-/*   Updated: 2024/04/21 23:57:54 by asepulve         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <ServerContext.hpp>
 
 ServerContext::ServerContext(std::string _name, std::string _ip, int _port) 
