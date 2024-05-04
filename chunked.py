039c7fd2f15cbeebb3e22e3989b90be696f16143
Arthur Aguiar <sepulven40028922@gmail.com>
2024-05-04 23:16:24 +0100

    Saving progress[...]

--- chunked.py
+++ chunked.py
@@ -1,16 +1,41 @@
+# import requests
+
+# url = 'http://127.0.0.1:8080'
+
+# # Define the data chunks
+# chunks = [
+#     '7\r\nMozilla\r\n',
+#     '11\r\nDeveloper Network\r\n',
+#     '0\r\n\r\n'  # Final empty chunk to indicate end of message body
+# ]
+
+# # Combine chunks into a single string
+# request_body = ''.join(chunks)
+
+# # Send the request with the entire body
+# headers = {
+#     'Content-Type': 'text/plain',
+#     'Transfer-Encoding': 'chunked'
+# }
+
+# response = requests.post(url, headers=headers, data=request_body)
+
+# # Print response
+# print(response.text)
+
 import requests
 
 url = 'http://127.0.0.1:8080'
 
 # Define the data chunks
 chunks = [
-    b'7\r\nMozilla\r\n',
-    b'11\r\nDeveloper Network\r\n',
-    b'0\r\n\r\n'  # Final empty chunk to indicate end of message body
+    '7\r\nMozilla\r\n',
+    '11\r\nDeveloper Network\r\n',
+    '0\r\n\r\n'  # Final empty chunk to indicate end of message body
 ]
 
-# Combine chunks into a single byte string
-request_body = b''.join(chunks)
+# Combine chunks into a single string
+request_body = ''.join(chunks)
 
 # Send the request with the entire body
 headers = {
